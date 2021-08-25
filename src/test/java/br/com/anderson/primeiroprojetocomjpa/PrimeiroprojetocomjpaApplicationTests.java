0fadc64d0a26a43f1a836b78fd97ac992d23ce64
Anderson Kuntz Meurer <andersonmeurerr@gmail.com>
2021-08-25 10:51:21 -0300

    Commit inicial
    Promeiro Commit

---
+++ src/test/java/br/com/anderson/primeiroprojetocomjpa/PrimeiroprojetocomjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.anderson.primeiroprojetocomjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroprojetocomjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
